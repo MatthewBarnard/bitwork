34c83a0eef39e34e782d009985f445f4761e36ff
Matthew Barnard <msbarnard10@gmail.com>
2016-04-04 21:37:47 +0200

    updated adverts

--- app/controllers/provider_adverts_controller.rb
+++ app/controllers/provider_adverts_controller.rb
@@ -11,6 +11,18 @@ class ProviderAdvertsController < ApplicationController
   end
 
   def save
-    render nothing: true
+    current_advert = Advert.new(advert_params)
+    current_advert.user_id = session[:user_id]
+    current_advert.is_active = true
+
+    if current_advert.valid?
+      current_advert.save
+    end
+
+    redirect_to '/provider/adverts'
+  end
+
+  def advert_params
+    params.require(:advert).permit(:id,:date_required,:date_expiry,:title,:payment,:description,:location)
   end
 end
