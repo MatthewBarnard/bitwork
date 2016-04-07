39450f1a831ded25722c467c427ea4247b78fe9b
Matthew Barnard <msbarnard10@gmail.com>
2016-04-07 21:35:31 +0200

    fixed registration bug

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -23,9 +23,19 @@ class FrontendController < ApplicationController
     end
 
     verified_login = User.login(user.email, user.password)
-    session[:user_id] = !verified_login ? nil : verified_login
-    session[:recent_login] = true
-
+    if !verified_login
+      session[:user_id] = nil
+      session[:recent_login] = nil
+      session[:account_link] = nil
+    else
+      session[:user_id] = verified_login
+      session[:recent_login] = true
+      if User.find(verified_login).user_type == 2
+        session[:account_link] = 'provider/profile'
+      else
+        session[:account_link] = 'seeker/profile'
+      end
+    end
 
     redirect_to action: 'index'
   end
@@ -73,6 +83,8 @@ class FrontendController < ApplicationController
     else
       session[:user_id] = verified_login
       session[:recent_login] = true
+
+
       if User.find(verified_login).user_type == 2
         session[:account_link] = 'provider/profile'
       else
